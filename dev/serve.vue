917c92b4e1b856cec3d0294fbbae6f87ad627357
martin.dietrich <martin.dietrich@muenchen.de>
2022-03-28 15:09:15 +0200

    First working draft

---
+++ dev/serve.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <VMultiFileInput
+      color="primary"
+      :schema="{}"
+      fullKey="allOf-0.allOf-0.antragsdaten.datumAntragstellung.currentOneOf.FormField03"
+      :rules="[]"
+      label="File"
+      apiEndpoint="api/digitalwf-backend-service"
+      s3Endpoint="s3"
+    ></VMultiFileInput>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Provide } from "vue-property-decorator";
+import { VMultiFileInput } from "@/entry.esm";
+@Component({
+  components: { VMultiFileInput },
+})
+export default class App extends Vue {
+
+  @Provide('contextId') contextId = 'Task01'
+}
+</script>

0494eee2cef9ceaf9b83084ea325533594cd30a5
martind260 <99957516+martind260@users.noreply.github.com>
2022-04-26 08:35:37 +0200

    pdf preview and readonly fix into master (#11)
    * Add first version of dependency license check (#6)
    
    * Feature/preview (#7)
    
    * working draft
    
    * adjust input-param
    
    * added form for testing rules
    
    * working draft
    
    * cleanup
    
    * validate file size
    
    * build errors fixed
    
    * Bool -> bool
    
    * format
    
    * export fixed
    
    * "no preview available"-text added
    
    * component wrap added
    
    * docu
    
    Co-authored-by: Dominik Horn <dominik.horn93@gmail.com>
    
    * Fix/required (#4)
    
    * working draft
    
    * adjust input-param
    
    * added form for testing rules
    
    Co-authored-by: Dominik Horn <dominik.horn93@gmail.com>
    
    * Fix/readonly (#10)
    
    * working draft
    
    * adjust input-param
    
    * added form for testing rules
    
    * working draft
    
    * cleanup
    
    * validate file size
    
    * build errors fixed
    
    * Bool -> bool
    
    * format
    
    * export fixed
    
    * "no preview available"-text added
    
    * component wrap added
    
    * docu
    
    * reset error on upload
    
    * readonly prop removed
    
    * added canAddDoc
    
    * added readonly-prop
    
    Co-authored-by: Dominik Horn <dominik.horn93@gmail.com>
    
    Co-authored-by: Andreas Zill <andreas@nerdy-by-nature.biz>
    Co-authored-by: Dominik Horn <dominik.horn93@gmail.com>

--- src/lib-components/VMultiFileInput.vue
+++ src/lib-components/VMultiFileInput.vue
@@ -15,45 +15,36 @@
     />
 
     <div v-if="documents && documents.length > 0" class="listWrapper">
-      <div v-for="doc in documents" :key="doc.name">
-        <v-card elevation="3" class="d-flex align-center linkline">
-          <a target="_blank" class="documentLink" @click="openInTab(doc)">
-            <v-img
-              v-if="isImage(doc)"
-              :src="doc.data"
-              class="mr-2"
-              max-height="200"
-              max-width="200"
-              alt="Image preview..."
-            />
-            <v-icon v-else size="30" class="mr-2">
-              {{ calculateIcon(doc.fileType) }}
-            </v-icon>
-            {{ doc.name }}
-          </a>
-          <v-spacer />
-          <template v-if="!readonly">
-            <v-btn class="removeButton" icon @click="removeDocument(doc.name)">
-              <v-icon> mdi-delete </v-icon>
-            </v-btn>
-          </template>
-        </v-card>
-      </div>
+      <template v-for="doc in documents">
+        <v-file-preview
+          :document="doc"
+          :key="doc.name"
+          :readonly="readonly"
+          @remove-document="removeDocument"
+        />
+      </template>
     </div>
-    <div class="errormessage">{{ errorMessage }}</div>
+    <div style="clear: both"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Emit, Inject, Prop, Vue } from "vue-property-decorator";
-import { VBtn, VFileInput, VIcon } from "vuetify/lib/components";
-import { HumanTaskFileRestControllerApiFactory, ServiceStartFileRestControllerApiFactory } from "@/api/api-client/api";
+import { VFileInput } from "vuetify/lib/components";
+import {
+  HumanTaskFileRestControllerApiFactory,
+  ServiceStartFileRestControllerApiFactory,
+} from "@/api/api-client/api";
 import FetchUtils from "@/api/FetchUtils";
 import mime from "mime";
 import globalAxios from "axios";
 import { DocumentData, FormContext } from "@/lib-components/types";
+import VFilePreview from "@/lib-components/VFilePreview.vue";
 
-@Component({ name: "VMultiFileInput", components: { VBtn, VFileInput, VIcon } })
+@Component({
+  name: "VMultiFileInput",
+  components: { VFileInput, VFilePreview },
+})
 export default class VMultiFileInput extends Vue {
   model = "";
   fileValue: File | null = null;
@@ -61,10 +52,12 @@ export default class VMultiFileInput extends Vue {
   documents: DocumentData[] = [];
   errorMessage = "";
   isLoading = false;
-  readonly = false;
 
   @Prop()
   valid: boolean | undefined;
+
+  @Prop()
+  readonly: boolean | undefined;
 
   @Prop()
   hasFocused: boolean | undefined;
@@ -96,11 +89,11 @@ export default class VMultiFileInput extends Vue {
   @Prop()
   rules: any | undefined;
 
-  @Inject('apiEndpoint')
+  @Inject("apiEndpoint")
   readonly apiEndpoint!: string;
 
-  @Inject('formContext')
-  readonly formContext!: FormContext
+  @Inject("formContext")
+  readonly formContext!: FormContext;
 
   @Emit()
   input(value: any): any {
@@ -108,8 +101,7 @@ export default class VMultiFileInput extends Vue {
   }
 
   created(): void {
-    this.readonly = this.schema.readOnly;
-    if (!this.formContext.id){
+    if (!this.formContext.id) {
       this.errorMessage = "no contextId";
       return;
     }
@@ -117,7 +109,7 @@ export default class VMultiFileInput extends Vue {
   }
 
   get isReadonly(): boolean {
-    return this.disabled || this.readonly || this.isLoading;
+    return this.disabled || this.readonly || this.schema.readOnly || this.isLoading || !this.canAddDocument;
   }
 
   get canAddDocument(): boolean {
@@ -134,6 +126,11 @@ export default class VMultiFileInput extends Vue {
         await this.loadFile(filename);
       }
       this.errorMessage = "";
+      if (this.documents.length > 0) {
+        // set dummy value to satisfy "required"-rule
+        this.fileValue = new File([""], this.documents[0].name);
+        this.input(this.documents);
+      }
     } catch (error) {
       this.errorMessage = "Die Dateien konnten nicht geladen werden.";
     }
@@ -168,21 +165,22 @@ export default class VMultiFileInput extends Vue {
     try {
       this.isLoading = true;
 
-      const presignedUrl = await this.getPresignedUrlForPost();
+      this.validateFileSize(mydata);
 
       const base64 = this.arrayBufferToBase64(mydata);
+      const presignedUrl = await this.getPresignedUrlForPost();
       await globalAxios.put(presignedUrl, base64);
 
       const doc = this.createDocumentDataInstance(
         this.fileValue!.name,
         this.fileValue!.type,
-        base64
+        base64,
+        mydata.byteLength
       );
 
       this.documents.push(doc);
 
       this.errorMessage = "";
-      this.fileValue = null;
       this.isLoading = false;
       this.input(this.documents);
     } catch (error: any) {
@@ -192,8 +190,8 @@ export default class VMultiFileInput extends Vue {
         error.response.status == 409
       ) {
         this.errorMessage = "Das Dokument existiert bereits.";
-      } else {
-        this.errorMessage = "Das Dokument konnte nicht hochgeladen werden.";
+      } else if (!this.errorMessage) {
+        this.errorMessage = "Das Dokument konnte nicht hochgeladen werden.: ";
       }
       setTimeout(() => {
         this.isLoading = false;
@@ -202,17 +200,34 @@ export default class VMultiFileInput extends Vue {
     this.isLoading = false;
   }
 
-  createDocumentDataInstance(name: string, type: string, data: string) {
+  validateFileSize(mydata: ArrayBuffer) {
+    if (mydata.byteLength > 10485760) {
+      this.errorMessage = "Die Datei ist muss kleiner als 10MB sein.";
+      throw new Error("File too large.");
+    }
+  }
+
+  createDocumentDataInstance(
+    name: string,
+    type: string,
+    data: string,
+    size?: number
+  ) {
+    if (!size) {
+      const content = atob(data);
+      size = content.length;
+    }
     const doc: DocumentData = {
       type: type,
       name: name,
-      data: this.toDataUrl(type, data)
-    }
+      data: this.toDataUrl(type, data),
+      size: size,
+    };
     return doc;
   }
 
   toDataUrl(type: string, data: string): string {
-    const str = "data:" + type + ";base64, " + data;
+    const str = `data:${type};base64, ${data}`;
     return str;
   }
 
@@ -222,13 +237,12 @@ export default class VMultiFileInput extends Vue {
     cfg.basePath += "/" + this.apiEndpoint;
 
     let res: any;
-    if (this.formContext.type === 'start'){
+    if (this.formContext.type === "start") {
       res = await ServiceStartFileRestControllerApiFactory(cfg).getFileNames1(
         this.formContext.id,
         this.getFullKey()
       );
-    }
-    else {
+    } else {
       res = await HumanTaskFileRestControllerApiFactory(cfg).getFileNames(
         this.formContext.id,
         this.getFullKey()
@@ -259,18 +273,21 @@ export default class VMultiFileInput extends Vue {
     cfg.basePath += "/" + this.apiEndpoint;
 
     let res: any;
-    if (this.formContext.type === 'start'){
-      res = await ServiceStartFileRestControllerApiFactory(cfg).getPresignedUrlForFileUpload1(
-      this.formContext.id,
-      this.getFullKey(),
-      this.fileValue!.name
+    if (this.formContext.type === "start") {
+      res = await ServiceStartFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileUpload1(
+        this.formContext.id,
+        this.getFullKey(),
+        this.fileValue!.name
       );
-    }
-    else {
-      res = await HumanTaskFileRestControllerApiFactory(cfg).getPresignedUrlForFileUpload(
-      this.formContext.id,
-      this.getFullKey(),
-      this.fileValue!.name
+    } else {
+      res = await HumanTaskFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileUpload(
+        this.formContext.id,
+        this.getFullKey(),
+        this.fileValue!.name
       );
     }
 
@@ -283,18 +300,21 @@ export default class VMultiFileInput extends Vue {
     cfg.basePath += "/" + this.apiEndpoint;
 
     let res: any;
-    if (this.formContext.type === 'start'){
-      res = await ServiceStartFileRestControllerApiFactory(cfg).getPresignedUrlForFileDownload1(
-      this.formContext.id,
-      this.getFullKey(),
-      filename
+    if (this.formContext.type === "start") {
+      res = await ServiceStartFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileDownload1(
+        this.formContext.id,
+        this.getFullKey(),
+        filename
       );
-    }
-    else {
-      res = await HumanTaskFileRestControllerApiFactory(cfg).getPresignedUrlForFileDownload(
-      this.formContext.id,
-      this.getFullKey(),
-      filename
+    } else {
+      res = await HumanTaskFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileDownload(
+        this.formContext.id,
+        this.getFullKey(),
+        filename
       );
     }
 
@@ -306,28 +326,33 @@ export default class VMultiFileInput extends Vue {
     cfg.basePath += "/" + this.apiEndpoint;
 
     let res: any;
-    if (this.formContext.type === 'start'){
-      res = await ServiceStartFileRestControllerApiFactory(cfg).getPresignedUrlForFileDeletion1(
-      this.formContext.id,
-      this.getFullKey(),
-      filename
+    if (this.formContext.type === "start") {
+      res = await ServiceStartFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileDeletion1(
+        this.formContext.id,
+        this.getFullKey(),
+        filename
       );
-    }
-    else {
-      res = await HumanTaskFileRestControllerApiFactory(cfg).getPresignedUrlForFileDeletion(
-      this.formContext.id,
-      this.getFullKey(),
-      filename
+    } else {
+      res = await HumanTaskFileRestControllerApiFactory(
+        cfg
+      ).getPresignedUrlForFileDeletion(
+        this.formContext.id,
+        this.getFullKey(),
+        filename
       );
     }
 
     return res.data;
   }
 
-  changeInput(/*fileInput: any*/) {
+  changeInput() {
     if (!this.fileValue) {
       return;
     }
+
+    this.errorMessage = '';
 
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -341,55 +366,19 @@ export default class VMultiFileInput extends Vue {
     reader.readAsArrayBuffer(this.fileValue);
   }
 
-  calculateIcon(type: string): string {
-    if (type === "application/pdf") {
-      return "mdi-file-pdf";
-    }
-    if (
-      type === "application/msword" ||
-      type ===
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-      type === "application/vnd.oasis.opendocument.text"
-    ) {
-      return "mdi-file-word";
-    }
-    if (
-      type === "application/vnd.ms-powerpoint" ||
-      type ===
-        "application/vnd.openxmlformats-officedocument.presentationml.presentation" ||
-      type === "application/vnd.oasis.opendocument.presentation"
-    ) {
-      return "mdi-file-powerpoint";
-    }
-    if (
-      type === "application/vnd.ms-excel" ||
-      type ===
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
-      type === "application/vnd.oasis.opendocument.spreadsheet"
-    ) {
-      return "mdi-file-excel";
-    }
-    if (
-      type === "application/xml" ||
-      type === "application/json" ||
-      type === "application/xhtml+xml" ||
-      type === "text/html" ||
-      type === "text/xml"
-    ) {
-      return "mdi-file-code";
-    }
-    return "mdi-file";
-  }
-
-  async removeDocument(filename: string): Promise<void> {
+  async removeDocument(document: DocumentData): Promise<void> {
     for (let i = 0; i < this.documents.length; i++) {
-      if (this.documents[i].name == filename) {
+      if (this.documents[i].name == document.name) {
         try {
           const presignedDeleteUrl = await this.getPresignedUrlForDelete(
-            filename
+            document.name
           );
           await globalAxios.delete(presignedDeleteUrl);
           this.documents.splice(i, 1);
+          if (this.documents.length == 0) {
+            // set null value to violate "required"-rule
+            this.fileValue = null;
+          }
           break; // only remove first item
         } catch (error) {
           this.errorMessage = "Die Datei konnte nicht gelöscht werden.";
@@ -397,46 +386,6 @@ export default class VMultiFileInput extends Vue {
       }
     }
     this.input(this.documents);
-  }
-
-  isImage(doc: DocumentData): boolean {
-    return (
-      doc.type.toLowerCase() === "image/jpeg" ||
-      doc.type.toLowerCase() === "image/png"
-    );
-  }
-
-  openInTab(doc: DocumentData): void {
-    const blobUrl = this.toBlobUrl(doc.data, doc.type!);
-    const link = document.createElement("a");
-    link.href = blobUrl;
-    link.setAttribute("download", doc.name!);
-    document.body.appendChild(link);
-    link.click();
-  }
-
-  toBlobUrl(base64Data: any, contentType: string): string {
-    const byteCharacters = atob(
-      base64Data.substr(`data:${contentType};base64,`.length)
-    );
-    const byteArrays = [];
-
-    for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
-      const slice = byteCharacters.slice(offset, offset + 1024);
-
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-
-      const byteArray = new Uint8Array(byteNumbers);
-
-      byteArrays.push(byteArray);
-    }
-    const blob = new Blob(byteArrays, { type: contentType });
-    const blobUrl = URL.createObjectURL(blob);
-
-    return blobUrl;
   }
 
   arrayBufferToBase64(buffer: ArrayBuffer) {
@@ -452,50 +401,18 @@ export default class VMultiFileInput extends Vue {
 </script>
 
 <style>
-/* remove messages from v-input to make adjacent list seamless  */
-.v-file-input .v-text-field__details {
+/* hide last added filename in textfield */
+.v-file-input .v-file-input__text {
   display: none;
-  margin-bottom: 0px;
-}
-.v-input__slot {
-  margin-bottom: -1px;
 }
 </style>
 
 <style scoped>
-.removeButton {
-  margin: 0;
-}
-
 .listWrapper {
-  overflow: auto;
-  z-index: 10;
-  margin-top: 0px;
-}
-
-.errormessage {
-  margin-bottom: 24px;
-  color: #ff5252 !important;
-  caret-color: #ff5252 !important;
-  font-size: 12px;
-  padding-left: 6px;
-  word-break: break-word;
-}
-
-.documentLink {
-  white-space: nowrap;
-  overflow: hidden;
+  margin-top: -6px;
+  margin-bottom: 26px;
+  float: left;
   display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.linkline {
-  margin: 2px 2px 2px 2px;
-  padding: 5px;
-}
-
-.linkline:hover {
-  background-color: #fafafa;
+  flex-wrap: wrap;
 }
 </style>

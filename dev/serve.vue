6ad172f21d7fb0fe046831fe8192e936967db194
martin.dietrich <martin.dietrich@muenchen.de>
2022-06-03 10:39:55 +0200

    cleanup serve.vue

--- dev/serve.vue
+++ dev/serve.vue
@@ -3,9 +3,8 @@
     <v-form ref="form">
     <VMultiFileInput
       color="primary"
-      :schema="{}"
-      htmlDescription="<p>Dies ist eine Beschreibung.</p>"
-      fullKey="allOf-0.allOf-0.antragsdaten.datumAntragstellung.currentOneOf.FormField03"
+      :schema='{"description": "Dies ist eine Beschreibung."}'
+      fullKey="FormField_Datei"
       :rules="['Required']"
       label="File"
     ></VMultiFileInput>
@@ -31,7 +30,7 @@ export default class App extends Vue {
 
   @Provide('formContext')
   formContext = {
-    id:'Task01',
+    id:'3a33d0d1-e253-11ec-9cf6-00059a3c7a00',
     type:'task'
   }
   @Provide('apiEndpoint') apiEndpoint = 'api/digitalwf-backend-service'
